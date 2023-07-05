84d4e1c504039e51bdf9623004906ff89c8959e8
Sudeep Virajith <virajithaabekoon@gmail.com>
2023-07-05 16:48:01 +0530

    build: exportGroupsToCsv added

--- src/group/functions/getAllGroups.ts
+++ src/group/functions/getAllGroups.ts
@@ -38,43 +38,46 @@ export async function getAllGroups() {
 }
 
 export async function exportGroupsToCsv() {
-  document.addEventListener('keypress', async (e) => {
-    if (e.key === 'g') {
-      let optionSet = {};
-      Swal.fire({
-        title: 'Extracting Available Groups',
-        text: 'Please Wait',
-        preConfirm: async () => {
-          const groups = await getAllGroups();
-          groups.map((onegroup) => {
-            let groupname = onegroup?.name!;
-            let id = onegroup?.id._serialized!;
-            console.log(groupname, id);
-            optionSet = { ...optionSet, [id]: groupname };
-          });
-          console.log(optionSet);
-        },
-        didOpen: () => {
-          Swal.showLoading();
-          Swal.clickConfirm();
-        },
-        showConfirmButton: false,
-      }).then(() => {
-        Swal.fire({
-          title: 'Select A Group To Continue',
-          input: 'select',
-          inputPlaceholder: 'Select Group',
-          inputOptions: optionSet,
-          inputAttributes: {
-            style: 'width:85%',
-          },
-        }).then(async (res) => {
-          console.log(res);
-          const participents = await getParticipants(res.value);
-          console.log(participents);
-        });
+  let optionSet = {};
+  Swal.fire({
+    title: 'Extracting Available Groups',
+    text: 'Please Wait',
+    preConfirm: async () => {
+      const groups = await getAllGroups();
+      groups.map((onegroup) => {
+        let groupname = onegroup?.name!;
+        let id = onegroup?.id._serialized!;
+        optionSet = { ...optionSet, [id]: groupname };
       });
-    }
+    },
+    didOpen: () => {
+      Swal.showLoading();
+      Swal.clickConfirm();
+    },
+    showConfirmButton: false,
+  }).then(() => {
+    Swal.fire({
+      title: 'Select A Group To Continue',
+      input: 'select',
+      inputPlaceholder: 'Select Group',
+      inputOptions: optionSet,
+      inputAttributes: {
+        style: 'width:85%',
+      },
+    }).then(async (res) => {
+      const participents = await getParticipants(res.value);
+      var exportfile = 'data:text/csv;charset=utf-8,';
+      participents?.participants.map((user) => {
+        try {
+          exportfile += user.replace('@c.us', '') + '\n';
+        } catch {}
+      });
+      var encodedUri = encodeURI(exportfile);
+      var link = document.createElement('a');
+      link.setAttribute('href', encodedUri);
+      link.setAttribute('download', `${res.value.replace('@g.us', '')}.csv`);
+      document.body.appendChild(link);
+      link.click();
+    });
   });
 }
-exportGroupsToCsv();
